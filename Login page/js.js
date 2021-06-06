89abcaaa585b0c2ed37e1f6218d55b487d8ab840
RahulYadav1011 <52218278+RahulYadav1011@users.noreply.github.com>
2021-06-06 17:41:58 +0530

    Add files via upload

---
+++ Login page/js.js
@@ -0,0 +1,26 @@
+function check(form)
+{
+
+   if(form.Name.value == "Rahul" && form.pwd.value == "Yadav")
+    {
+      window.open('index.html')
+    }
+   else
+   {
+     alert("Invalid User Name or password")
+    }
+}
+   function logout()
+{
+  alert("Visnu Bhai Hoo Gya Logout");
+}
+function submit(){
+  alert("New Registration is Successful");
+  location.href = "login.html";
+}
+function signup(){
+  location. href = "/signup.html";
+}
+function rahul(){
+  alert="succesful registered";
+}
